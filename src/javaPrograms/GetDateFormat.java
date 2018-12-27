a705f1882497e6d5a8a0e7bdb7d4ba2f62fafedc
riyazullasharief <riyazullasharief@gmail.com>
2018-12-27 13:13:28 +0530

    update

---
+++ src/javaPrograms/GetDateFormat.java
@@ -0,0 +1,16 @@
+package javaPrograms;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GetDateFormat
+{
+	public static void main(String[] args)
+	{
+		DateFormat formatDate=new SimpleDateFormat("dd/MM/YYYY");
+		Date date=new Date();
+		String date1=formatDate.format(date);
+		System.out.println(date1);
+	}
+}
